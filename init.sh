bee95bb039dfe3d7070e8c84059a847f2702d17b
rdinesh6 <34704276+rdinesh6@users.noreply.github.com>
2020-03-25 13:42:19 +0530

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+##
+# BASH script that checks the following:
+# demo-task is repo dir from master##
+set -xv
+export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
+
+find /root/demo/demo-task | entr sh /script/cpFiles.sh
