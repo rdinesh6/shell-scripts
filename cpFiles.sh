cace2cf12cb6e8d6fa1211e1e4d3779d428a7d77
rdinesh6 <34704276+rdinesh6@users.noreply.github.com>
2020-03-26 18:06:43 +0530

    Delete cpFiles.sh

--- cpFiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-##
-Repo_Path=/root/demo/demo-task
-cp $Repo_Path/file1.txt $Repo_Path/file2.txt  $Repo_Path/test1
-cp $Repo_Path/file2.txt $Repo_Path/file4.txt  $Repo_Path/test2
-cp $Repo_Path/file3.txt $Repo_Path/file4.txt $Repo_Path/file5.txt  $Repo_Path/test3
-sh script/git-commit.sh 
